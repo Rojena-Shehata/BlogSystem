bc07af89cf7c53cc1e965acb0388dfa011363560
Rojena Shehata <rojenashehata@gmail.com>
2025-12-19 23:37:54 +0200

    Implement Unit Of Work Pattern

---
+++ BlogSystem.Domain/Contrcts/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BlogSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Domain.Contrcts
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, Tkey> GetRepository<TEntity,Tkey>(TEntity entity) where TEntity:BaseEntity<Tkey>;
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ BlogSystem.Presistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using BlogSystem.Domain.Contrcts;
+using BlogSystem.Domain.Entities;
+using BlogSystem.Presistence.Data.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Presistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        Dictionary<string,object> _repositories = [];
+        private readonly BlogDbContext _dbContext;
+
+        public UnitOfWork(BlogDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGenericRepository<TEntity, Tkey> GetRepository<TEntity, Tkey>() where TEntity : BaseEntity<Tkey>
+        {
+            var entityType=typeof(TEntity).Name;
+            if(_repositories.TryGetValue(entityType, out var newRepository))
+            {
+                return (IGenericRepository<TEntity,Tkey>)newRepository;
+            }
+            var Repository = new GenericRepository<TEntity, Tkey>(_dbContext);
+            _repositories.Add(entityType, Repository);
+            return Repository;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+          return await  _dbContext.SaveChangesAsync();
+        }
+    }
+}
